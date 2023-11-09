d9498e3ff0bebe38aae09864c755607bf02a42ee
socratesmendes <socrates.mendes7@hotmail.com>
2023-11-08 21:47:14 -0300

    useStates

--- Frontend/frontend/src/pages/Consulta/index.js
+++ Frontend/frontend/src/pages/Consulta/index.js
@@ -1,13 +1,48 @@
+import React, { useState } from "react";
 import './index.css';
 import { Link } from 'react-router-dom';
 
 function Consulta() {
+
+  const[formProduto, setFormProduto] = useState(false)
+  const[formCliente, setFormCliente] = useState(false)
+  const[formCategoria, setFormCategoria] = useState(false)
+  const[formTipo, setFormTipo] = useState(false)
+
+  const handleFormProduto = () => {
+    setFormProduto(!formProduto);
+    setFormCliente(false);
+    setFormCategoria(false);
+    setFormTipo(false);
+  }
+
+  const handleFormCliente = () => {
+    setFormProduto(false);
+    setFormCliente(!formCliente);
+    setFormCategoria(false);
+    setFormTipo(false);
+  }
+
+  const handleFormCategoria = () => {
+    setFormProduto(false);
+    setFormCliente(false);
+    setFormCategoria(!formCategoria);
+    setFormTipo(false);
+  }
+
+  const handleFormTipo = () => {
+    setFormProduto(false);
+    setFormCliente(false);
+    setFormCategoria(false);
+    setFormTipo(!formTipo);
+  }
+
   return (    
     <div>
 
-      <h1>.Ease</h1>
+      <nav>
 
-        <nav>
+        <h1>.Ease</h1>
 
         <ul>
 
@@ -16,17 +51,57 @@ function Consulta() {
           </li>
 
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/cadastro">Cadastro</Link>
           </li>
-          
+
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/consulta">Consulta</Link>
+          </li>
+
+          <li>
+            <Link to="/vendas">Vendas</Link>
           </li>
 
         </ul>
 
       </nav>
-      
+
+      <div className='buttons'>
+
+        <button onClick={handleFormProduto}>Produto</button>
+        {formProduto && (
+          <form>
+            <input type="text" />
+            <button>Enviar</button>
+          </form>
+        )}
+
+        <button onClick={handleFormCliente}>Cliente</button>
+        {formCliente && (
+          <form>
+            <input type="text" />
+            <button>Enviar</button>
+          </form>
+        )}
+
+        <button onClick={handleFormCategoria}>Categoria</button>
+        {formCategoria && (
+          <form>
+            <input type="text" />
+            <button>Enviar</button>
+          </form>
+        )}
+
+        <button onClick={handleFormTipo}>Tipo</button>
+        {formTipo && (
+          <form>
+            <input type="text" />
+            <button>Enviar</button>
+          </form>
+        )}
+
+      </div>
+
     </div>
   );
 }
